1786fd40f95f4bcd43de6c2bad70982452b8193b
Im25n <imengasmi200@gmail.com>
2025-03-03 13:25:02 +0100

    cambiamento aggiunto nella cartella do index3.html e esercizio3.js

---
+++ esercizio3.js
@@ -0,0 +1,23 @@
+function CalcoloArea()  
+     let forma = prompt ("inserisci la forma di cui vuoi calcolare l'area:rettangolo, triangolo,cerchio")
+console.log(forma)
+
+// imposta le regole per l forme geometriche 
+
+
+if ( forma === "rettangolo"){
+    // logica calcolo area rettangolo
+    console.log ("calcolo forma rettangolo IN")
+ const base = prompt ("inserisci il valore della base")
+const altezza = prompt ("inserisci dell' altezza ") 
+console.log ("calcolo forma rettangolo valori bh" + base + altezza)
+} 
+if (forma === "triangolo") {
+//logica calcolo area triangolo
+console.log("calcolo forma traingolo IN")
+
+}
+if (forma === "cerchio")  {
+    //logica calcolo area cerchio
+    console.log ("calcolo forma cerchio IN")
+}
